60142ab6a058cb913d303d6c385cdf3d826c4c71
Matej Gace <matej.gace@t.ht.hr>
2022-10-17 14:36:45 +0200

    My first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Person firstPerson = new Person("Pedro0");
+        Person firstPerson = new Person("Pedro0","Croatia");
+        System.out.println(firstPerson.getName());
     }
 }
