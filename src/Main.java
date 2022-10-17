e18841803b43612cbb40f4b170c79d75b36ad99f
matejgace <115802571+matejgace@users.noreply.github.com>
2022-10-17 14:52:02 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Person firstPerson = new Person("Pedro0","Croatia");
         System.out.println(firstPerson.getName());
+        System.out.println(firstPerson.getCountry());
     }
 }
