bb011792a67c0ebcd6cf592b469c9aa5cc4a0484
Matej Gace <matej.gace@t.ht.hr>
2022-10-17 16:01:06 +0200

    My first branch commit

---
+++ src/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    private String name;
+    private String breed;
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+}
